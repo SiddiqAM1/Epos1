c6a576a69eb66bfb6c55a57ecff0dfbebb7f44d7
Muhammad Siddiq <ahmedmuhammadsiddiq4@gmail.com>
2024-01-31 21:42:53 +0500

    Second Commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: 'Epos Realty',
-  description: 'Welcome to Epos Realty, where over 11 years of real estate expertise meet your property journey. With us, every property tells a unique story, and we're here to help you find yours. Explore homes with character and investments with potential. Get ready for seasoned advice, market insights, and a personalized approach to real estate. Because at Epos Realty, where every story begins here. For personalized consultations and property details, contact us at:',
+  description: "Welcome to Epos Realty, where over 11 years of real estate expertise meet your property journey. With us, every property tells a unique story, and w're here to help you find yours. Explore homes with character and investments with potential. Get ready for seasoned advice, market insights, and a personalized approach to real estate. Because at Epos Realty, where every story begins here. For personalized consultations and property details, contact us at:",
 }
 
 export default function RootLayout({
